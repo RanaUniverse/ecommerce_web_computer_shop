4529465adb3b7b3704b67b859d4f5a7050c4dfee
RanaUniverse <RanaUniverse321@gmail.com>
2026-05-05 14:02:46 +0530

    Now i make a demo main.py i added pycache=no, sot that runnign python will not make pycache folder

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from ecommerce-web-computer-shop!")
+"""
+main.py
+Here this file will my entrypoint
+i will try to run this and it will run a basic app for checking
+"""
 
+# This upper two line help to stop making the .pyc file to looks clean the repo
+import sys
 
-if __name__ == "__main__":
-    main()
+sys.dont_write_bytecode = True
+
+# Now from below i will write my main logic of code
