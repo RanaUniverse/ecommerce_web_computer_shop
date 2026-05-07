f33a46d80980515c6515c286d45276314a465a04
RanaUniverse <RanaUniverse321@gmail.com>
2026-05-07 10:37:43 +0530

    import some functions of database operation related int he init.py

--- services/database/controllers/__init__.py
+++ services/database/controllers/__init__.py
@@ -3,3 +3,13 @@ services/database/controllers/__init__.py
 I will make the functions related to the
 data insert, update or delete in this package
 """
+
+from .category import add_one_category_row
+from .product import add_one_product_row
+from .user import add_new_user_row
+
+__all__ = [
+    "add_new_user_row",
+    "add_one_category_row",
+    "add_one_product_row",
+]
