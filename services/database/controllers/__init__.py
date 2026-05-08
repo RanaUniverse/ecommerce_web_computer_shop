7f0925ee5ad37627a095d91db259812bc9931627
RanaUniverse <RanaUniverse321@gmail.com>
2026-05-08 08:06:46 +0530

    some user obj retrive functions has been import in this package for easy use

--- services/database/controllers/__init__.py
+++ services/database/controllers/__init__.py
@@ -6,10 +6,20 @@ data insert, update or delete in this package
 
 from .category import add_one_category_row
 from .product import add_one_product_row
-from .user import add_new_user_row
+from .user import (
+    add_new_user_row,
+    get_user_row_by_email_id,
+    get_user_row_by_phone_no,
+    get_user_row_by_username,
+    get_user_row_by_user_id,
+)
 
 __all__ = [
     "add_new_user_row",
     "add_one_category_row",
     "add_one_product_row",
+    "get_user_row_by_email_id",
+    "get_user_row_by_phone_no",
+    "get_user_row_by_username",
+    "get_user_row_by_user_id",
 ]
