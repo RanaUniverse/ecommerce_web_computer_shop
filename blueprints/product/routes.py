2b051bfd8b928230a17e8f21ad0e830caa5c018f
RanaUniverse <RanaUniverse321@gmail.com>
2026-05-17 18:33:37 +0530

    images will saved in its own folder for the products

--- blueprints/product/routes.py
+++ blueprints/product/routes.py
@@ -6,6 +6,9 @@ I will use to store the images in the static folders
 Because the images are meant to shows alwyas easily
 """
 
+from pathlib import Path
+
+
 from flask import (
     Blueprint,
     render_template,
@@ -13,6 +16,10 @@ from flask import (
     url_for,
     redirect,
 )
+
+from werkzeug.datastructures import FileStorage
+from werkzeug.utils import secure_filename
+
 
 from .forms import ProductAddForm
 
@@ -28,6 +35,8 @@ from services.database.controllers import (
 from services.database.models import (
     ProductModel,
 )
+
+from utils.config import PRODUCT_IMAGE_UPLOAD_ROOT
 
 product_bp = Blueprint(
     name="product_bp",
@@ -77,6 +86,7 @@ def add_product():
     form = ProductAddForm()
     form.brand_id.choices = [("", "Select Brand")] + get_all_brands_id_name()  # type: ignore
     list_of_category = get_all_category_names()
+
     if form.validate_on_submit():  # type: ignore
         name = form.name.data
         description = form.description.data
@@ -88,7 +98,6 @@ def add_product():
         price_purchase = form.purchase_price.data
         price_sell = form.sell_price.data
         price_mrp = form.mrp_price.data
-        print(brand_id)
 
         if not category_name:
             category_id = None
@@ -126,8 +135,6 @@ def add_product():
                     items=list_of_category,
                 )
 
-        print(21212121212)
-        print(brand_obj)
         # the decimal and float problem i need to solve later in postgres change to decimal
         new_product_obj = add_one_product_row(
             product_obj=ProductModel(
@@ -149,7 +156,23 @@ def add_product():
                 category="error",
             )
             return redirect(url_for("product_bp.add_product"))
+
+        # this else part comes means the product creation has successfull
         else:
+            # this time i will need to create the folder to insert the image
+            thumbnail_file: FileStorage = form.image_thumbnail.data
+            if thumbnail_file.filename:
+                product_folder = PRODUCT_IMAGE_UPLOAD_ROOT / f"{new_product_obj.id_}"
+                product_folder.mkdir(
+                    parents=True,
+                    exist_ok=True,
+                )
+                original_filename = secure_filename(thumbnail_file.filename)
+                file_extension = Path(original_filename).suffix.lower()
+                image_path = product_folder / f"thumbnail{file_extension}"
+                # i dont need to validate the extension as this is done by wtforms
+                thumbnail_file.save(image_path)
+
             flash(
                 message="Created product",
                 category="success",
