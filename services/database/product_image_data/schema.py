86daf1c4a96d357bb903e83c12fdb9cd0977b828
RanaUniverse <RanaUniverse321@gmail.com>
2026-05-26 18:29:59 +0530

    make a schema for the gallery images so that it will be easy to pass what i need

--- services/database/product_image_data/schema.py
+++ services/database/product_image_data/schema.py
@@ -20,3 +20,17 @@ class ProductThumbnailImageOut(SQLModel):
     # filepath: str | None = None
     # alt_text: str | None = None
     # external_url: str | None = None
+
+
+class ProductGalleryImageOut(SQLModel):
+    """
+    this is the minimal information for the gallery images
+    later i will want admin will pass external link for the images to have
+    shows in the gallery image not directly from my disk
+    """
+
+    display_order: int
+    
+    filepath: str | None = Field(default=None)
+    external_url: str | None = Field(default=None)
+    alt_text: str | None = Field(default=None)
