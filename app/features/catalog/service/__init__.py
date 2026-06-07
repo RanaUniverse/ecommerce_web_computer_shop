22fb551b6a35c8bc7e682b55c92ad84dcd568c09
RanaUniverse <RanaUniverse321@gmail.com>
2026-06-08 00:55:11 +0530

    make service things in a new folder

---
+++ app/features/catalog/service/__init__.py
@@ -0,0 +1,6 @@
+"""
+app/features/catalog/service/__init__.py
+
+Here i will make the image of service which will do the convertion
+from schema to model and to schema and work directly call by routes
+"""
