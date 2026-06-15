54a519a195c878aa550e17020b7c3f448ea2bf6a
RanaUniverse <RanaUniverse321@gmail.com>
2026-06-15 18:17:10 +0530

    just little infromaotn of the db fucntion

--- app/features/catalog/operations/__init__.py
+++ app/features/catalog/operations/__init__.py
@@ -5,4 +5,9 @@ The dataabse operations i will keep write here in this place
 
 Note:
     Operations should only do with Models not with the schema.
+
+
+Later i need to make a simplse class with DI,
+so that this one will help to make normal CRUD with id_ for all the tables
+
 """
